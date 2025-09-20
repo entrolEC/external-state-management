7da38ed0f21e441476f27f08645cea6d6ad7d2ad
Euichan Jung <euichan1108@gmail.com>
2025-09-20 17:48:31 +0900

    feat: 안내 문구 추가

--- src/app/example/2/page.tsx
+++ src/app/example/2/page.tsx
@@ -19,9 +19,10 @@ export default function Example2() {
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold">Example2</h1>
         <h2 className="mt-4 text-lg font-semibold">useRef를 사용하여 구현</h2>
-        <div ref={widthRef} className="mt-8 rounded-md bg-yellow-200 px-4 py-2 font-medium">
+        <div ref={widthRef} className="mt-20 rounded-md bg-yellow-200 px-4 py-2 font-medium">
           {typeof window !== 'undefined' && `width: ${window.innerWidth}`}
         </div>
+        <div className="mt-4 text-xs text-gray-800">* 윈도우의 크기를 조절하여 변화를 확인해보세요.</div>
       </div>
     </div>
   );
--- src/app/example/3/page.tsx
+++ src/app/example/3/page.tsx
@@ -17,7 +17,8 @@ export default function Example3() {
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold">Example3</h1>
         <h2 className="mt-4 text-lg font-semibold">useSyncExternalStore를 사용하여 구현</h2>
-        <div className="mt-8 rounded-md bg-green-200 px-4 py-2 font-medium">width: {width}</div>
+        <div className="mt-20 rounded-md bg-green-200 px-4 py-2 font-medium">width: {width}</div>
+        <div className="mt-4 text-xs text-gray-800">* 윈도우의 크기를 조절하여 변화를 확인해보세요.</div>
       </div>
     </div>
   );
